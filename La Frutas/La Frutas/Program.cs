66b54b62144739f0dfa42bebb021ac9e31776084
hughmikem22 <50379908+hughmikem22@users.noreply.github.com>
2019-07-21 18:56:49 -0500

    Add files via upload

---
+++ La Frutas/La Frutas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace La_Frutas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruits FRT1 = new Fruits("Banana", "Yellow","Like a Sausage",4.8);
+
+            FRT1.choice();
+            FRT1.pick();
+            FRT1.requestWeight();
+        }
+
+    }
+}
